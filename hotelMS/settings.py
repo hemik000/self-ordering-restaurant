6f6cd243d8cf3315c6c9e422ff4f670183592c50
Hemik Vijay <hemikkamli@gmail.com>
2020-09-10 20:54:28 +0530

    Update settings.py

--- hotelMS/settings.py
+++ hotelMS/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = "+pt)uw-$_kf_!o2&5d#4v)c584=^44-6q@-7r1fl_fyzm2xb8@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.getenv("env") == "prod":
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ["hotelms.herokuapp.com", "127.0.0.1", "192.168.0.100"]
 
