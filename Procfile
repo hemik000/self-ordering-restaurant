1c57b4958f834063932038d12cf592c38f08919b
Hemik Vijay <hemikkamli@gmail.com>
2020-09-10 20:45:06 +0530

    heroku-fix

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn hotelMS.wsgi --log-file -
--- hotelMS/settings.py
+++ hotelMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "+pt)uw-$_kf_!o2&5d#4v)c584=^44-6q@-7r1fl_fyzm2xb8@"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["hotelms.herokuapp.com", "127.0.0.1", "192.168.0.100"]
 
 
 # Application definition
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     # Third Party
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -105,6 +106,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -113,9 +118,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
@@ -136,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
@@ -170,4 +181,3 @@ REST_FRAMEWORK = {
         "drf_renderer_xlsx.renderers.XLSXRenderer",
     ],
 }
-
